0f0ff451c9e214f73a5162ecc1bb80b2d777ab5e
Angelo Panariti <angpanariti@gmail.com>
2024-10-27 17:34:32 +0100

    LSP Troubleshooting

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -3,46 +3,10 @@
     enable = true;
       servers = {
         tsserver.enable = true; # TS/JS
-        cssls.enable = true; # CSS
-        tailwindcss.enable = true; # TailwindCSS
-        html.enable = true; # HTML
-        phpactor.enable = true; # PHP
-        svelte.enable = false; # Svelte
         pyright.enable = true; # Python
-        marksman.enable = true; # Markdown
         nil-ls.enable = true; # Nix
-        dockerls.enable = true; # Docker
-        bashls.enable = true; # Bash
-        clangd.enable = true; # C/C++
-        csharp-ls.enable = true; # C#
-        yamlls.enable = true; # YAML
 
-        lua-ls = { # Lua
-          enable = true;
-          settings.telemetry.enable = false;
-        };
 
-        # Rust
-        rust-analyzer = {
-          enable = true;
-          installRustc = false;
-          installCargo = false;
-          settings = {
-            completion.fullFunctionSignatures.enable = true;
-            checkOnSave = true;
-            check = {command = "clippy";};
-            procMacro = {enable = true;};
-            lens = {
-              implementations.enable = true;
-              references = {
-                method.enable = true;
-                adt.enable = true;
-                trait.enable = true;
-                enumVariant.enable = true;
-              };
-        };
-      };
-    };
   };
 };
 }
